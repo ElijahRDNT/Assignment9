860a53386ccd4f3adda42d0e4e297596062b7721
LAPTOP-7O8QBV3P\lenovo <elijahmaetardiente@iskolarngbayan.pup.edu.ph>
2022-02-10 20:57:03 +0800

    Added the picture, full name, and course. Also added formal designs for good visuality.

--- PDF_Resume_Creator.py
+++ PDF_Resume_Creator.py
@@ -1,3 +1,8 @@
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib import colors
 import json
 
 #read the json file
@@ -8,8 +13,31 @@ def read_data():
     obj = json.loads(jsondata)
     return obj
 
+def create_pdf(path, obj):
+    my_canvas = canvas.Canvas(path, pagesize=letter)
+    my_canvas.setLineWidth(.3)
+    my_canvas.setFillColor(colors.HexColor("#d3d3d3"))  #gray bg
+    my_canvas.rect(0, 669, 1000, 500, stroke=0, fill=1)     #gray box above
+
+    my_canvas.setFillColor(colors.HexColor("#cd853f"))
+    my_canvas.rect(145, 677, 378, 56, stroke=0, fill=1) # brown border of white box
+    my_canvas.setFont('Arial(Body)', 18)                #setting of font
+    my_canvas.setFillColor(colors.HexColor("#fefefa"))  #white color
+    my_canvas.rect(145, 680, 375, 50, stroke=0, fill=1)   #white rectangle
+    my_canvas.setFillColor(colors.HexColor("#555555"))      #gray color for full name
+    my_canvas.drawString(185, 707, str(obj["full_name"]))   #fullname
+    my_canvas.setFont('Arial(Body)', 9)
+    my_canvas.drawString(258, 690, str(obj["course"]))      #course below full name
+    my_canvas.setStrokeColor(colors.HexColor("#cd853f"))    #brown color stroke
+    my_canvas.setFillColor(colors.HexColor("#cd853f"))      #brown color fill
+    my_canvas.rect(63, 648, 104, 104, stroke=1, fill=1)     #brown rectangle border for applicant picture
+    my_canvas.drawImage(str(obj["image"]), 65, 650, width=100, height=100)  #image
+    my_canvas.save()
 
 
+pdfmetrics.registerFont(TTFont('Arial(Body)', 'Arial.ttf'))
+pdfmetrics.registerFont(TTFont('Arial(Body) - Bold', 'Arialbd.ttf'))
 obj = read_data()
 
-print(str(obj["full_name"]))
+if __name__ == '__main__':
+    create_pdf('ARDIENTE_ELIJAHMAE.pdf', obj)
